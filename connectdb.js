5c28a2afa4087b03a252751cded8e46e6d4b3097
fjfernandez93 <paco.fernandez.muelas@gmail.com>
2016-12-01 18:03:42 +0100

    check variable entorno docker

--- connectdb.js
+++ connectdb.js
@@ -4,7 +4,12 @@
 var pgp = require('pg-promise')();
 if(process.env.CONNECT_WITH_SSL=="OK")
   pgp.pg.defaults.ssl = true;
-var db = pgp(process.env.DBDATA);
 
+
+if(process.env.PG_TEST_PORT_5432_TCP_ADDR){
+  var db = pgp('postgres://docker:docker@'+process.env.PG_TEST_PORT_5432_TCP_ADDR+':5432/docker');
+}else{
+  var db = pgp(process.env.DBDATA);
+}
 
 exports.db = db;
