cd99b0b9ddd3a137c318afb2dab487ed4c76cb15
fjfernandez93 <paco.fernandez.muelas@gmail.com>
2017-01-29 20:20:15 +0100

    añadido archivo con tareas para Flightplan close #18

---
+++ flightplan.js
@@ -0,0 +1,19 @@
+// flightplan.js
+var plan = require('flightplan');
+
+// configuration
+plan.target('staging', {
+  host: 'crimson-meadow-38.westeurope.cloudapp.azure.com',
+  username: 'vagrant',
+  privateKey: '/Users/paco/.ssh/id_rsa',
+  passphrase: proces.env.PKPASSPHR,
+  agent: process.env.SSH_AUTH_SOCK
+});
+
+plan.remote(function(remote) {
+  remote.log('Move folder to web root');
+
+  remote.exec('git clone https://github.com/fjfernandez93/ProyectoIV.git')
+  remote.exec('forever stop ~/ProyectoIV/bin/www', {failsafe: true});
+  remote.exec('forever start  ~/ProyectoIV/bin/www');
+});
